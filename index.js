a74bbf9d20ca9da35ceae614f9e2bf4de2c6f4e4
hyung-2 <nh_0221@naver.com>
2023-08-11 18:06:57 +0900

    다시 테스트

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const productsRouter = require('./src/routes/products')
 
 
 const corsOptions = {
-  origin: 'http://127.0.0.1:5500',
+  origin: 'https://hyung-2.github.io',
   credentials: true
 }
 
